b93c19bbc4d6675f07f789e770d750360ad40d74
yen0x <gege.dert@gmail.com>
2014-04-02 00:09:11 +0200

    added flash success on user creation and sign in controller action

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -8,6 +8,8 @@ import java.security.spec.InvalidKeySpecException;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import play.Logger;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.db.jpa.Transactional;
 import play.libs.Json;
@@ -23,9 +25,11 @@ public class UserController extends Controller {
 
 	@Transactional
 	public static Result joinAsGuest() {
+		//TODO add session connection
 		ObjectNode result = Json.newObject();
 		User guest = User.createGuest();
 		if (null != guest) {
+			result.put("type", "guest login");
 			result.put("user", guest.username);
 			result.put("password", guest.password);
 		}
@@ -45,12 +49,37 @@ public class UserController extends Controller {
 			message+=" avec le nom d'utilisateur " + user.username + ".\r\n \r\n";
 			message+="A bientôt dans les salons Mymoz !";
 			EmailHelper.sendSimpleEmail("Inscription Mymoviequiz", user.email, message);
-			return redirect(routes.Application.index("signupOK"));
+			flash("success", "Votre compte a &eacute;t&eacute; cr&eacute;&eacute; ! <a href='"+routes.Application.game()+"' class='alert-link'>Jouez en cliquant ici !</a>");
+			return redirect(routes.Application.index());
 		}
 	}
 	
 
 	public static Result signup() {
 		return ok(signup.render(userForm));
+	}
+	
+	@Transactional
+	public static Result signin(){
+		DynamicForm requestData = Form.form().bindFromRequest();
+        String username = requestData.get("username");
+        String password = requestData.get("password");
+		User user = User.findByUsername(username);
+		boolean authenticated = false;
+		try {
+			if(null != user){
+				authenticated = user.authenticate(password);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			Logger.error("NoSuchAlgorithmException");
+		} catch (InvalidKeySpecException e) {
+			Logger.error("InvalidKeySpecException");
+		}
+		
+		ObjectNode result = Json.newObject();
+		result.put("type", "authentication");
+		result.put("authenticated", authenticated);
+	
+		return ok(result);
 	}
 }
